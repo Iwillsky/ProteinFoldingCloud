c3847a238c18c66f1472a7f10b92383edcbde882
iwillsky <45908192+Iwillsky@users.noreply.github.com>
2021-10-08 09:32:26 +0800

    Upd

--- data/PollSubmitJob.sh
+++ data/PollSubmitJob.sh
@@ -4,7 +4,9 @@ python3 jobpoll.py
 export ISARRIVING=`awk -F '=' '/\[jobpoll\]/{a=1}a==1&&$1~/arriving/{print $2;exit}' hackjob.ini`
 export SEQSTR=`awk -F '=' '/\[jobpoll\]/{a=1}a==1&&$1~/seqstr/{print $2;exit}' hackjob.ini`
 export CURJOBID=`awk -F '=' '/\[jobpoll\]/{a=1}a==1&&$1~/curjobid/{print $2;exit}' hackjob.ini`
-echo $SEQSTR>>"input"$CUR_JOBID".fa"
+fnameinput="in"${CURJOBID}".fa"
+echo $fnameinput
+#echo $SEQSTR>>fnameinput
 echo $CURJOBID
 echo $ISARRIVING
 #sbatch runjob.sh $CURJOBID
