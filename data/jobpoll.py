50f304be797084776ec564430fc09cc9c3b47f65
iwillsky <45908192+Iwillsky@users.noreply.github.com>
2021-10-10 12:24:56 +0800

    Update Oct10
    Job scripts update

--- data/jobpoll.py
+++ data/jobpoll.py
@@ -33,7 +33,7 @@ def job_poll():
             max_item_count=1
         ))
         os.environ['PROTEIN_POLL_ARRIVING']='1'
-        os.environ['PROTEIN_POLL_SEQSTR']=items[0].get("strseq")
+        os.environ['PROTEIN_POLL_SEQSTR']=items[0].get("inputfilestring")
         os.environ['PROTEIN_POLL_DOCID']=items[0].get("id")
         os.environ['PROTEIN_POLL_CURJOBID']=items[0].get("jobid")
         os.environ['PROTEIN_POLL_INPUTURL']=items[0].get("inputblobURL")
@@ -41,7 +41,7 @@ def job_poll():
         cf = configparser.ConfigParser()
         cf.add_section("jobpoll")
         cf.set("jobpoll", "arriving", "1")
-        cf.set("jobpoll", "seqstr", items[0].get("strseq"))
+        cf.set("jobpoll", "seqstr", items[0].get("inputfilestring"))
         cf.set("jobpoll", "curjobid", items[0].get("jobid"))
         with open("hackjob.ini","w+") as f:
             cf.write(f)
@@ -54,108 +54,9 @@ def job_poll():
     finally:
         print("\nPoll exit")
 
-
-def job_tag(strjobid):
-    client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY}, user_agent="CosmosDBPythonQuickstart", user_agent_overwrite=True)
-    try:
-        db = client.get_database_client(DATABASE_ID)
-        print('Database with id \'{0}\' opened'.format(DATABASE_ID))
-
-        container = db.get_container_client(CONTAINER_ID)
-        print('Container with id \'{0}\' opened'.format(CONTAINER_ID))                          
-        
-        items = list(container.query_items(        
-            query="SELECT * FROM r WHERE r.jobid=@jobid",
-            parameters=[
-                { "name":"@jobid", "value": strjobid }
-            ],               
-            enable_cross_partition_query=True,
-            max_item_count=1
-        ))
-        docid = items[0].get("id")
-
-        read_item = container.read_item(item=docid, partition_key=strjobid)
-        read_item['state'] = 'RUNNING'
-        response = container.replace_item(item=read_item, body=read_item)
-        os.environ['PROTEIN_POLL_ARRIVING']='0'
-
-        print("Job updated as RUNNING")
-        
-    except exceptions.CosmosHttpResponseError as e:
-        print('\nJob state update caught an error. {0}'.format(e.message))
-
-    finally:
-        print("\nJob update exit")
-
-
-def job_refreshstatus(strjobid, strstatus):
-    client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY}, user_agent="CosmosDBPythonQuickstart", user_agent_overwrite=True)
-    try:
-        db = client.get_database_client(DATABASE_ID)
-        print('Database with id \'{0}\' opened'.format(DATABASE_ID))
-
-        container = db.get_container_client(CONTAINER_ID)
-        print('Container with id \'{0}\' opened'.format(CONTAINER_ID))        
-
-        items = list(container.query_items(        
-            query="SELECT * FROM r WHERE r.jobid=@jobid",
-            parameters=[
-                { "name":"@jobid", "value": strjobid }
-            ],               
-            enable_cross_partition_query=True,
-            max_item_count=1
-        ))
-        docid = items[0].get("id")
-        
-        read_item = container.read_item(item=docid, partition_key=strjobid)
-        read_item['statusinfo'] = strstatus
-        response = container.replace_item(item=read_item, body=read_item)
-
-        print("Job status refreshed.")
-        
-    except exceptions.CosmosHttpResponseError as e:
-        print('\nJob status refresh caught an error. {0}'.format(e.message))
-
-    finally:
-        print("\nJob refresh exit")
-
-
-def job_pushresult(strjobid, urlBlobResult):
-    client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY}, user_agent="CosmosDBPythonQuickstart", user_agent_overwrite=True)
-    try:
-        db = client.get_database_client(DATABASE_ID)
-        print('Database with id \'{0}\' opened'.format(DATABASE_ID))
-
-        container = db.get_container_client(CONTAINER_ID)
-        print('Container with id \'{0}\' opened'.format(CONTAINER_ID))        
-
-        items = list(container.query_items(        
-            query="SELECT * FROM r WHERE r.jobid=@jobid",
-            parameters=[
-                { "name":"@jobid", "value": strjobid }
-            ],               
-            enable_cross_partition_query=True,
-            max_item_count=1
-        ))
-        docid = items[0].get("id")
-        
-        read_item = container.read_item(item=docid, partition_key=strjobid)
-        read_item['state'] = 'COMPLETED'
-        read_item['resultblobURL'] = urlBlobResult
-        response = container.replace_item(item=read_item, body=read_item)
-
-        print("Job result pushed")
-        
-    except exceptions.CosmosHttpResponseError as e:
-        print('\nJob result push caught an error. {0}'.format(e.message))
-
-    finally:
-        print("\nJob result push exit")
-
-
 if __name__ == '__main__':
     job_poll()       
-    #=items[0].get("id")
+    
     #job_tag(sys.arg[1])
     #job_tag(os.environ['PROTEIN_POLL_DOCID'], os.environ['PROTEIN_POLL_CURJOBID'])
 
--- data/polljob.sh
+++ data/polljob.sh
@@ -4,11 +4,12 @@ python3 jobpoll.py
 export ISARRIVING=`awk -F '=' '/\[jobpoll\]/{a=1}a==1&&$1~/arriving/{print $2;exit}' hackjob.ini`
 export SEQSTR=`awk -F '=' '/\[jobpoll\]/{a=1}a==1&&$1~/seqstr/{print $2;exit}' hackjob.ini`
 export CURJOBID=`awk -F '=' '/\[jobpoll\]/{a=1}a==1&&$1~/curjobid/{print $2;exit}' hackjob.ini`
-fnameinput="in"${CURJOBID}".fa"
-echo $fnameinput
-#echo $SEQSTR>>fnameinput
+export INPUT_FILENAME=input$(date "+%Y%m%d%H%M%S").fa
+echo $INPUT_FILENAME
+echo $SEQSTR>>$INPUT_FILENAME
 echo $CURJOBID
 echo $ISARRIVING
-#sbatch runjob.sh $CURJOBID
+#sbatch runjob.sh $CURJOBID $INPUT_FILENAME
 echo "sbatch job..."
-#python3 jobtag.py $CUR_JOBID
+python3 jobtag.py $CUR_JOBID
+echo "job updated."
