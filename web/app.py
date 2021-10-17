28123cda15b75bd4304d7859ea912eadfe71affb
iwillsky <45908192+Iwillsky@users.noreply.github.com>
2021-10-17 21:01:49 +0800

    Update1017

--- web/app.py
+++ web/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # encoding: utf-8
 from flask import Flask, request, render_template
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
 from azure.cosmos import CosmosClient
 import os
 import uuid
+import hashlib
 
 app = Flask(__name__, static_url_path = "/static", static_folder = "static")
 
@@ -14,6 +16,13 @@ database_name = 'hackjoblist'
 database = client.get_database_client(database_name)
 container_name = 'joblist'
 container = database.get_container_client(container_name)
+
+# Intentionally left blank to not make this public
+storage_account_name = os.environ.get('STORAGE_ACCOUNT_NAME', 'mshack2021storage')
+storage_account_key = os.environ.get('STORAGE_ACCOUNT_KEY', '')
+storage_connection_string = "DefaultEndpointsProtocol=https;AccountName={};AccountKey={};EndpointSuffix=core.windows.net".format(storage_account_name, storage_account_key)
+
+blob_service_client = BlobServiceClient.from_connection_string(storage_connection_string)
 
 # root endpoint
 @app.route("/", methods=["GET", "POST"])
@@ -27,6 +36,11 @@ def upload_file():
         # read file line by line and append each found sequence to list
         f = request.files['file_name']
         line = f.readline().decode("utf-8").strip()
+
+        storage_container_name = str(uuid.uuid4())
+        container_client = blob_service_client.create_container(storage_container_name)
+        file_upload_name = str(uuid.uuid4()) + ".txt"
+
         while line:
             line_count += 1
             if line_count >= max_lines:
@@ -42,6 +56,19 @@ def upload_file():
                     else:
                         sequence['value'] += line
             line = f.readline().decode("utf-8").strip()
+
+        # Instantiate a new BlobClient to interact with the blob
+        blob_client = blob_service_client.get_blob_client(container=storage_container_name, blob=file_upload_name)
+
+        print("\nUploading to Azure Storage as blob:\n\t" + file_upload_name)
+        blob_client.upload_blob(f.read())
+        print("\nListing blobs...")
+
+        # List the blobs in the container
+        blob_list = container_client.list_blobs()
+        for blob in blob_list:
+            print("\t" + blob.name)
+
         # get the cursor positioned at end
         f.seek(0, os.SEEK_END)
         # this will be equivalent to size of file
